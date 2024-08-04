04d2d57639327a774a7c8f029e734204b7e4fc3d
Mariana Lopez <marianaalexa76@gmail.com>
2024-08-04 17:20:30 -0600

    update: se cambiaron todos los imports de jakarta.persistence en clase Order por jakarta.persistence, *; para que agregara todos

--- olga_franco/src/main/java/com/olgafranco/app/model/Orders.java
+++ olga_franco/src/main/java/com/olgafranco/app/model/Orders.java
@@ -3,19 +3,7 @@ package com.olgafranco.app.model;
 import java.util.Date;
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="orders")

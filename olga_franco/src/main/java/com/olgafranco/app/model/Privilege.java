c0f5097fd9dd8faadfd398e1014883342b4f1c27
Mariana Lopez <marianaalexa76@gmail.com>
2024-08-04 19:45:51 -0600

    add: se agrego clase Privilege con su controller y repository

--- olga_franco/src/main/java/com/olgafranco/app/model/Privilege.java
+++ olga_franco/src/main/java/com/olgafranco/app/model/Privilege.java
@@ -1,62 +1,50 @@
 package com.olgafranco.app.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name="roles")
+@Table(name = "privileges")
 public class Privilege {
-	
-	@Id 
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(name="name", length=45, nullable=false)
-	private String name;
-	
-	@Column(name="description", length=150, nullable=true)
-	private String description;
 
-	public Privilege() {}
-	
-	public Privilege(Long id, String name, String description) {
-		this.id = id;
-		this.name = name;
-		this.description = description;
-	}
-	
-	public static Privilege setUserRole(int roleId) {
-		return new Privilege(  (long) roleId, "", "");
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	public Long getId() {
-		return id;
-	}
+    @Column(name = "name", length = 45, nullable = false)
+    private String name;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @Column(name = "description", length = 150, nullable = true)
+    private String description;
 
-	public String getName() {
-		return name;
-	}
+    public Privilege() {}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Privilege(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	} 
-	
-	
-	
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
